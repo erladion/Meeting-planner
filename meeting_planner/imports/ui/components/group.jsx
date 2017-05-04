d10f07bc7dd913384ad72a89fff7cc501168a4b6
Filip Magnusson <filma035@student.liu.se>
2017-05-04 11:43:46 +0200

    Add style to profile page, you can now add memebers to your group

--- meeting_planner/imports/ui/components/group.jsx
+++ meeting_planner/imports/ui/components/group.jsx
@@ -18,12 +18,27 @@ export class Group extends React.Component{
         if (!groupInfo) groupInfo = {name:"",description:""};
         console.log(groupInfo);
         return(
-            <div>
-            <h1>{groupInfo.name}</h1>
-            <h3>{groupInfo.description}</h3>
-
+            <div style={{width:'50%'}} className="w3-border">
+                <div className="w3-panel w3-border-bottom">
+                    <h3 className="w3-margin">{groupInfo.name}</h3>
+                    <h4>{groupInfo.description}</h4>
+                </div>
+                <div className="w3-panel w3-border w3-leftbar w3-border-blue w3-margin-left">
+                    <h4>Add member</h4>
+                    <input className="w3-input w3-border" type="text" width="200" value={this.state.newMemberName} onChange={this.saveMemberName}></input>
+                    <button className="w3-button w3-blue w3-margin-bottom" color="#2196F3" width="200" height="30" onClick={this.addMember}>Add </button>
+                </div>
             </div>
         )
+    }
+
+    saveMemberName(evt){
+        this.setState({newMemberName:evt.target.value});
+    }
+
+    addMember(){
+        Meteor.call("groups.addMember", this.state.groupInfo._id, this.state.newMemberName);
+        this.setState({newMemberName:""});
     }
 
     componentDidMount(){
--- meeting_planner/imports/ui/components/profile.jsx
+++ meeting_planner/imports/ui/components/profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+    import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { Tracker } from 'meteor/tracker'
 
@@ -16,7 +16,7 @@ export class Profile extends React.Component{
         var username = Meteor.user() ? (Meteor.user().username != "" ? Meteor.user().username : Meteor.user().services.google.email) : "";
         var image = Meteor.user() ? Meteor.user().services.google.picture : "";
         return(
-            <div>
+            <div style={{width:'50%'}} className="w3-border">
                 <div className="w3-panel w3-border-bottom">
                     <img style={{float: "left"}} src={image} width="150px" className="w3-margin"/>
                     <h3 style={{float: "left"}} className="w3-margin">{username}</h3>
@@ -24,7 +24,7 @@ export class Profile extends React.Component{
                 <div className="w3-panel w3-border w3-leftbar w3-border-blue w3-margin-left">
                     <h4>Change username</h4>
                     Current username: { username }
-                    <input className="w3-input w3-border" type="text" width="200" value={this.state.username} onChange={this.saveUsername}></input>
+                    <input className="w3-input w3-border" type="text" value={this.state.username} onChange={this.saveUsername}></input>
                     <button className="w3-button w3-blue w3-margin-bottom" color="#2196F3" width="200" height="30" onClick={this.changeUsername}>Change username</button>
                 </div>
                 <div className="w3-panel w3-border w3-leftbar w3-border-blue w3-margin-left">
@@ -45,6 +45,7 @@ export class Profile extends React.Component{
 
     addGroup(){
         Meteor.call("groups.insert", this.state.newGroupName);
+        this.setState({newGroupName: ""});
     }
 
     saveUsername(evt){
@@ -58,6 +59,6 @@ export class Profile extends React.Component{
     changeUsername(){
         var self = this;
         Meteor.call("users.changeUsername",this.state.username);
-        this.setState(this.state);
+        this.setState({username: ""});
     }
 }
