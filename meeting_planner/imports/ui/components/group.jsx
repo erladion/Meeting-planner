f29ca4d5400e028ff5ba07a533ec4cb151163a13
erladion <johan.jansson94@gmail.com>
2017-05-04 11:36:53 +0200

    Updated the group component with constructor as well as remove and add functions for events

--- meeting_planner/imports/ui/components/group.jsx
+++ meeting_planner/imports/ui/components/group.jsx
@@ -5,9 +5,18 @@ import { browserHistory } from 'react-router'
 import { Groups } from '../../api/groups/Groups'
 
 export class Group extends React.Component{
+    constructor(props){
+        super(props);
+        this.createEvent = this.createEvent.bind(this);
+        this.saveMemberName = this.saveMemberName.bind(this);
+        this.addMember = this.addMember.bind(this);
+        this.state = {groupInfo: {}, newMemberName: ''};
+    }
+
     render(){
         var groupInfo = Groups.findOne({_id:this.props.params.name});
         if (!groupInfo) groupInfo = {name:"",description:""};
+        console.log(groupInfo);
         return(
             <div>
             <h1>{groupInfo.name}</h1>
@@ -18,10 +27,29 @@ export class Group extends React.Component{
     }
 
     componentDidMount(){
+        this.setState({groupInfo: Groups.findOne({_id:this.props.params.name})});
         Tracker.autorun(() => {
             if (Meteor.user())
             this.forceUpdate();
         });
     }
 
+    createEvent(name, location, startTime, endTime, description){
+        var groupId = this.state.groupInfo._id;
+        var creatorId = Meteor.user().email;
+        var eventOjb = {
+            name: name,
+            creator: creatorId,
+            location: location,
+            startTime: startTime,
+            endTime: endTime,
+            description: description || "",
+        };
+        Meteor.call('groups.addEvent', groupId, eventObj);
+    }
+
+    removeEvent(eventId){
+        var groupId = this.state.groupInfo._id;
+        Meteor.call('removeEvent', groupId, eventId);
+    }
 }
